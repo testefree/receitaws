08beda5a578ef69a2f78f09c6d1dfba2d393e03d
5653ccee2d5271416d0001a9 <vagtonaf@hotmail.com>
2017-07-06 11:56:58 -0300

    Add project files.

---
+++ receitaws/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace receitaws.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
